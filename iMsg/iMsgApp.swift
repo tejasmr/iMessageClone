85ddf6a93bc2926a600275d700391f83818b19e4
tezz-io <matblockgreninja@gmail.com>
2020-10-19 19:57:24 +0530

    Initial Commit

---
+++ iMsg/iMsgApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMsgApp.swift
+//  iMsg
+//
+//  Created by Tejas M R on 19/10/20.
+//
+
+import SwiftUI
+
+@main
+struct iMsgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
